df7a5b803aa52cf0ad731ff7baa231c2ad7c3fb5
César Torres <ctorresc@unizar.es>
2023-04-15 13:36:21 +0200

    Code Revision 15/04/2023
    Continue Graph Results implementation

--- Examples/rorc/cGraphCost.m
+++ Examples/rorc/cGraphCost.m
@@ -1,4 +1,4 @@
-classdef cGraphSummary < cStatusLogger
+classdef cGraphCost < cStatusLogger
 % cGraphSummary plot as bar summary tables
     properties(GetAccess=public,SetAccess=private)
         Name        % Name of the graph (window name)
@@ -13,16 +13,20 @@ classdef cGraphSummary < cStatusLogger
     end
 
     methods
-        function obj = cGraphSummary(tbl,idx)
-        % Create the object with the graph properties
-            obj.Name='Cost Summary';
-            obj.Title=tbl.Description;
+        function obj = cGraphCost(tbl)
+        % Create the object with the graph properties      
+            obj.Name=tbl.Description;
+            obj.Title=[tbl.Description,' [',tbl.State,']'];
             obj.Categories=tbl.ColNames(2:end);
             obj.xValues=(1:tbl.NrOfCols-1)';
-            obj.yValues=cell2mat(tbl.Data(idx,:))';
-            obj.xLabel='States';
+            obj.yValues=circshift(cell2mat(tbl.Data(1:end-1,1:end)),1)';
+            if tbl.isFlowsTable
+                obj.xLabel='Flows';
+            else
+                obj.xLabel='Processes';
+            end
             obj.yLabel=['Unit Cost ',tbl.Unit];
-            obj.Legend=tbl.RowNames(idx);
+            obj.Legend={'ENV',tbl.RowNames{1:end-2}};
             if tbl.isGeneralCostTable || tbl.isFlowsTable
                 obj.BaseLine=0.0;
             else
@@ -35,7 +39,7 @@ classdef cGraphSummary < cStatusLogger
             f=figure('name',obj.Name, 'numbertitle','off', ...
                 'units','normalized','position',[0.1 0.1 0.45 0.6],'color',[1 1 1]);
             ax=axes(f);
-            bar(obj.xValues,obj.yValues,'edgecolor','none','barwidth',0.8,'parent',ax);
+            bar(obj.yValues,'stacked','edgecolor','none','barwidth',0.5,'parent',ax); 
             title(ax,obj.Title,'fontsize',14);
             tmp=ylim;yl(1)=obj.BaseLine;yl(2)=tmp(2);ylim(yl);
             set(ax,'xtick',obj.xValues,'xticklabel',obj.Categories,'fontsize',12);
@@ -45,7 +49,7 @@ classdef cGraphSummary < cStatusLogger
             set(ax,'xgrid','off')
             box(ax,'on');
             hl=legend(obj.Legend);
-            set(hl,'Orientation','horizontal','Location','southoutside');
+            set(hl,'location','northeastoutside','orientation','vertical');
         end
     end
 end
